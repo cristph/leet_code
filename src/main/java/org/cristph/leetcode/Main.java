bb25420989adc2bd3da8ef3945838658a45373ce
cristph.wxk <cristph.wxk@alibaba-inc.com>
2024-11-12 10:38:07 +0800

    init

---
+++ src/main/java/org/cristph/leetcode/Main.java
@@ -0,0 +1,11 @@
+package org.cristph.leetcode;
+
+/**
+ * @author cristph
+ * @date 2024/11/12
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
